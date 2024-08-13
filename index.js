de2010bc6451649050a145194341c773afae43fb
Vikash Nayan <imvikashnayan@gmail.com>
2024-08-13 17:55:56 +0530

    route done

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import router from "./routes/posts.js";
+
+const app = express();
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use("/post", router);
+
+const PORT = process.env.PORT || 5000;
+
+const MONGODB_URI =
+  "mongodb+srv://vikashnayan06:G9JVso7mvu8yYHPr@cluster0.xy1oz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() =>
+    app.listen(PORT, () => console.log(`server is running at port ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));

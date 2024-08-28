1368184db75ebc063cebe9ba447957f372b54ee8
Vikash Nayan <imvikashnayan@gmail.com>
2024-08-28 11:24:56 +0530

    fix cors

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import router from "./routes/posts.js";
 const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 app.use("/post", router);
 
 const PORT = process.env.PORT || 5000;
